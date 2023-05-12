680acfd8ba707138e598ab3547437641828344da
Jocemir <69916938+jocemir@users.noreply.github.com>
2023-05-12 11:06:04 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/br/edu/materdei/ponto/exercicio09052023/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.edu.materdei.ponto.marcando_ponto
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:marcando_ponto/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const PontoEletronico());
+}
+
+class PontoEletronico extends StatelessWidget {
+  const PontoEletronico({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Ponto Eletrônico',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.lightGreen,
+      ),
+      home: HomePage(),
+    );
+  }
+}
